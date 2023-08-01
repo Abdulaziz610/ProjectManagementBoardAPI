d28743f209d46bc132bc5ef0f6ce41c663850c38
Abdulaziz610 <Abdulaziz.tech@gmail.com>
2023-08-01 10:43:52 +0400

    Project Setup.

---
+++ src/main/java/com/BoardAPI/ProjectManagementBoardAPI/ProjectManagementBoardApiApplication.java
@@ -0,0 +1,13 @@
+package com.BoardAPI.ProjectManagementBoardAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementBoardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementBoardApiApplication.class, args);
+	}
+
+}
