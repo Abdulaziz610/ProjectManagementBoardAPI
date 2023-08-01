69f11fc95e4574800a522394a6f64e3767816274
Abdulaziz610 <Abdulaziz.tech@gmail.com>
2023-08-01 10:51:41 +0400

    Creating Card Model.

---
+++ src/main/java/com/BoardAPI/ProjectManagementBoardAPI/Models/CardModel.java
@@ -0,0 +1,20 @@
+package com.BoardAPI.ProjectManagementBoardAPI.Models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+
+public class CardModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    String title;
+    String description;
+    String Section;
+}
